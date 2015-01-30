b0e72f29d0d212c564d2dba02545f2d31189911f
Valentin Hăloiu <vially.ichb@gmail.com>
2015-01-30 15:57:22 +0200

    Add initial install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+ln -s ~/.config/vim/vimrc ~/.vimrc
+ln -s ~/.config/vim/gvimrc ~/.gvimrc
+ln -s ~/.config/git/gitconfig ~/.gitconfig
+ln -s ~/.config/xmobar/config ~/.xmobarrc
+ln -s ~/.config/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
