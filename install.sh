0ccc262a788d06f1c7947a6ca67cfca61b69536a
Valentin Hăloiu <vially.ichb@gmail.com>
2016-02-19 03:16:25 +0200

    Fix xmobar config path in install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ln -s ~/.config/git/gitconfig ~/.gitconfig
 ln -s ~/.config/pam/environment ~/.pam_environment
-ln -s ~/.config/xmobar/config ~/.xmobarrc
+ln -s ~/.config/xmobar/config.hs ~/.xmobarrc
 ln -s ~/.config/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 ln -s ~/.config/xsession/xkbmap ~/.Xkbmap
 ln -s ~/.config/xsession/xprofile ~/.xprofile
