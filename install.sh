959a5c0b8d653565fa38c0ee693f2f473ab67934
Valentin Hăloiu <vially.ichb@gmail.com>
2016-04-28 21:06:02 +0300

    Git already follows XDG

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-ln -s ~/.config/git/gitconfig ~/.gitconfig
 ln -s ~/.config/pam/environment ~/.pam_environment
 ln -s ~/.config/xmobar/config.hs ~/.xmobarrc
 ln -s ~/.config/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
