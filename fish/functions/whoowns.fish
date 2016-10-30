63ccf626812c2a0ddddade3c33ff30e89a75ee22
Valentin Hăloiu <vially.ichb@gmail.com>
2016-10-30 12:49:59 +0200

    [fish] Fix test condition in whoowns function

--- fish/functions/whoowns.fish
+++ fish/functions/whoowns.fish
@@ -1,6 +1,6 @@
 function whoowns
     set -l file_name $argv[1]
-    if test -e $file_name
+    if not test -e $file_name
         set file_name (command --search $file_name)
     end
     pacaur -Qo $file_name
