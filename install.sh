fd64d808aaad6345de3aa2e499940589a7819494
Valentin Hăloiu <vially.ichb@gmail.com>
2017-12-10 20:58:57 +0100

    [xmonad] Remove no longer used xmonad config files

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
 
 ln -s ~/.config/pam/environment ~/.pam_environment
-ln -s ~/.config/xmobar/config.hs ~/.xmobarrc
-ln -s ~/.config/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-ln -s ~/.config/xsession/xkbmap ~/.Xkbmap
 ln -s ~/.config/xsession/xprofile ~/.xprofile
